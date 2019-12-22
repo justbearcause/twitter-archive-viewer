5fbf4acafab4448d99d21c6fc362480078bd6739
Alexander Litvinenko <a.litvinenko@outlook.com>
2019-12-22 10:47:57 +0300

    Add layout component

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./global.css";
+import styles from "./layout.module.css";
+
+const Layout = ({ children }: { children: any }) => (
+  <main className={styles.centerColumn}>
+    {children}
+  </main>
+);
+
+export default Layout;
