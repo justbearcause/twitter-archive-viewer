8138eab0c550c3fed50abfc5c5c67aa5257aaec9
Alexander Litvinenko <a.litvinenko@outlook.com>
2019-12-22 13:05:49 +0300

    Add an ability to search tweets by tweet's text

--- src/components/tweets/tweets.tsx
+++ src/components/tweets/tweets.tsx
@@ -6,6 +6,7 @@ import Account from "../../models/twitter/account";
 
 import styles from "./tweets.module.css";
 import Profile from "../../models/twitter/profile";
+import { SearchIcon } from "../icons/icons";
 
 interface TweetsProps {
   tweets: TweetModel[];
@@ -20,6 +21,13 @@ export default (props: TweetsProps) => {
 
   const [filteredTweets, setFilteredTweets] = useState(filterDataSource());
 
+  const fetchMoreListItems = useCallback(() => {
+    setState(prevState => ({
+      items: [...prevState.items, ...filteredTweets.slice(prevState.items.length, Math.min(filteredTweets.length, prevState.items.length + 20))],
+      hasMoreItems: Math.min(filteredTweets.length, prevState.items.length + 20) < filteredTweets.length,
+    }));
+  }, [filteredTweets]);
+
   const [state, setState] = useState({
     items: filteredTweets.slice(0, Math.min(filteredTweets.length, 20)),
     hasMoreItems: Math.min(filteredTweets.length, 20) < filteredTweets.length,
@@ -29,21 +37,26 @@ export default (props: TweetsProps) => {
 
   useEffect(() => setFilteredTweets(filterDataSource()), [filter, filterDataSource])
 
-  useEffect(() => { setState({ items: [], hasMoreItems: true }); fetchMoreListItems(); }, [ filteredTweets ]);
-
-  function fetchMoreListItems() {
-    setState(prevState => ({
-      items: [...prevState.items, ...filteredTweets.slice(prevState.items.length, Math.min(filteredTweets.length, prevState.items.length + 20))],
-      hasMoreItems: Math.min(filteredTweets.length, prevState.items.length + 20) < filteredTweets.length,
-    }));
-  }
+  useEffect(() => { setState({ items: [], hasMoreItems: true }); fetchMoreListItems(); }, [filteredTweets, fetchMoreListItems]);
 
   return (
     <>
-      <input type="text" value={filter} onChange={e => setFilter(e.target.value)} />
+      <div className={styles.searchContainer}>
+        <div className={styles.searchWrapper}>
+          <input
+            type="text"
+            placeholder="Search tweets"
+            className={styles.searchField}
+            value={filter}
+            onChange={e => setFilter(e.target.value)}
+          />
+          <div className={styles.searchIconContainer}>
+            <SearchIcon className={styles.searchIcon} />
+          </div>
+        </div>
+      </div>
       <div className={styles.tweets}>
         {state.items.map(tweet => <Tweet key={tweet.id} tweet={tweet} author={props.account} profile={props.profile} />)}
-        {isFetching && <div>Is Fetching</div>}
       </div>
     </>
   );
