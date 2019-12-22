6ef0c26c4f847e67b944b4a09e6dc1123811daef
Alexander Litvinenko <a.litvinenko@outlook.com>
2019-12-22 10:48:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './App.module.css';
+import Home from './components/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <Home />
 }
 
 export default App;
