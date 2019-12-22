6c9c9625c1deb21b038f690668d20e7732a006a2
Alexander Litvinenko <a.litvinenko@outlook.com>
2019-12-22 12:24:44 +0300

    Add an ability to search tweets by tweet's text (POC)

--- src/components/tweets/tweets.tsx
+++ src/components/tweets/tweets.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Tweet from "../tweet/tweet";
 import useInfiniteScroll from "../../hooks/useInfiniteScroll";
 import TweetModel from "../../models/twitter/tweet";
@@ -14,24 +14,37 @@ interface TweetsProps {
 }
 
 export default (props: TweetsProps) => {
+  const [filter, setFilter] = useState("");
+
+  const filterDataSource = useCallback(() => props.tweets.filter(tweet => tweet.full_text.indexOf(filter) >= 0), [props.tweets, filter]);
+
+  const [filteredTweets, setFilteredTweets] = useState(filterDataSource());
+
   const [state, setState] = useState({
-    items: props.tweets.slice(0, Math.min(props.tweets.length, 20)),
-    hasMoreItems: Math.min(props.tweets.length, 20) < props.tweets.length,
+    items: filteredTweets.slice(0, Math.min(filteredTweets.length, 20)),
+    hasMoreItems: Math.min(filteredTweets.length, 20) < filteredTweets.length,
   });
 
   const [isFetching] = useInfiniteScroll(fetchMoreListItems);
 
+  useEffect(() => setFilteredTweets(filterDataSource()), [filter, filterDataSource])
+
+  useEffect(() => { setState({ items: [], hasMoreItems: true }); fetchMoreListItems(); }, [ filteredTweets ]);
+
   function fetchMoreListItems() {
     setState(prevState => ({
-      items: [...prevState.items, ...props.tweets.slice(prevState.items.length, Math.min(props.tweets.length, prevState.items.length + 20))],
-      hasMoreItems: Math.min(props.tweets.length, prevState.items.length + 20) < props.tweets.length,
+      items: [...prevState.items, ...filteredTweets.slice(prevState.items.length, Math.min(filteredTweets.length, prevState.items.length + 20))],
+      hasMoreItems: Math.min(filteredTweets.length, prevState.items.length + 20) < filteredTweets.length,
     }));
   }
 
   return (
-    <div className={styles.tweets}>
-      {state.items.map(tweet => <Tweet key={tweet.id} tweet={tweet} author={props.account} profile={props.profile} />)}
-      {isFetching && <div>Is Fetching</div>}
-    </div>
+    <>
+      <input type="text" value={filter} onChange={e => setFilter(e.target.value)} />
+      <div className={styles.tweets}>
+        {state.items.map(tweet => <Tweet key={tweet.id} tweet={tweet} author={props.account} profile={props.profile} />)}
+        {isFetching && <div>Is Fetching</div>}
+      </div>
+    </>
   );
 };
