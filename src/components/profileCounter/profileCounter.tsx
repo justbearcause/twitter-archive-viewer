e2d83045f616d3cc3f115e1d86dd933049ab2c5a
Alexander Litvinenko <a.litvinenko@outlook.com>
2019-12-22 10:45:54 +0300

    Add profile counter component

---
+++ src/components/profileCounter/profileCounter.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./profileCounter.module.css";
+
+interface Props {
+  counter: number;
+  text: string;
+};
+
+export default (props: Props) => {
+  return (
+    <div className={styles.profileCounter}>
+      <div className={styles.profileCounterNumber}>
+        {props.counter}
+      </div>
+      <div className={styles.profileCounterText}>
+        {props.text}
+      </div>
+    </div>
+  );
+};
